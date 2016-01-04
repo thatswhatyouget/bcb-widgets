6288e5c50bca85236ae40087407047b83cd087c2
Dave Rhodes <dave.was.r@gmail.com>
2016-01-04 04:19:19 +0000

    fixed countdown to automatically switch events when one expires

--- countdown/countdown.js
+++ countdown/countdown.js
@@ -5,13 +5,18 @@ function bcbCountdown(element, config) {
             var parts = line.split('~');
             return { time: new Date(parts.pop()), title: parts.shift(), subtitle: parts.join('~') };
         }).filter(function (event) { return event.time.valueOf() > Date.now(); })[0];
-        if (!event) return;
+        if (!event) return $(element).hide();
         var $timeLeft = $('<div>').addClass('timeLeft');
         $(element).addClass('bcbCountdown').append($('<div>').addClass('title').text(event.title || "New Episode"))
             .append($('<div>').addClass('subtitle').text(event.subtitle || "The Feels Awaken"))
             .append($timeLeft).addClass('style' + Math.floor(Math.random() * styles));
         function updateTime() {
             var duration = (event.time.valueOf() - Date.now()) / 1000;
+            if (duration < 0) {
+                clearInterval(interval);
+                $(element).find('.title, .subtitle, .timeLeft').hide();
+                return bcbCountdown(element, config);
+            }
             $timeLeft.html("<span>" + [24, 60, 60, 1].map(function (num, base, segments) {
                 for (var d = duration, i = base; i < segments.length; i++) d /= segments[i];
                 if (base) d %= segments[base - 1];
@@ -19,7 +24,7 @@ function bcbCountdown(element, config) {
             }).join("</span>:<span>") + "</span>");
         }
         updateTime();
-        setInterval(updateTime, 1000);
+        var interval = setInterval(updateTime, 1000);
     }
     catch (e) {
         if (typeof(debug) !== "undefined" && debug) {
