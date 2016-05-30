a32eefa64845de56627b0b1e62e1cf1b95d3ced9
Dave Rhodes <dave.was.r@gmail.com>
2016-05-30 15:32:14 +0000

    Removed negative scores, added stars. Changed Instructions and Game Over screen to give a gem breakdown. Gameplay should now be complete

--- hitthediamond/htd.js
+++ hitthediamond/htd.js
@@ -1,19 +1,21 @@
 function HitTheDiamond(selector) {
     var $game = $(selector).addClass('bcb-hit-the-diamond');
 
-    var score = 0, highscore = parseInt(window.localStorage.getItem('htd-highscore') || '0');
+    var stars = 10;
+
+    var score = 0, highscore = parseInt(window.localStorage.getItem('htd-highscore') || '0'), fails = 0;
 
     function addScore(points) {
         score += points;
-        if (score > highscore) {
-            highscore = score;
-            window.localStorage.setItem("htd-highscore", highscore);
-        }
-        $game.attr('data-highscore', highscore).attr('data-score', score);
+        $game.attr('data-score', score);
         if (score < 0) GameOver();
     }
 
-    addScore(0);
+    function addFail() {
+        fails++;
+        $game.find('.stars span:not(.used)').first().addClass('used');
+        if (fails > stars) GameOver();
+    }
 
     $game.mousedown(function () {
         $game.addClass('mouseDown');
@@ -27,13 +29,14 @@ function HitTheDiamond(selector) {
 
     function Gem(name, score, img, hitImg) {
         img = img || score;
-        score = parseInt(score) || -10;
+        score = parseInt(score) || 0;
         var safename = name.replace(/[^A-Z0-9]/ig, '');
         this.name = name;
         this.img = img;
         this.hitImg = hitImg || img;
         this.score = score;
         this.toString = function () { return safename; };
+        this.hits = 0;
     }
     var gems = [
         new Gem('Yellow Diamond', 7, "hitthediamond/img/gems/yellow-diamond.png", "hitthediamond/img/gems/yellow-diamond-hit.png"),
@@ -81,6 +84,12 @@ function HitTheDiamond(selector) {
     }
 
     function Setup() {
+        if (!$game.find('.stars').is('*')) {
+            var $stars = $('<div class="stars">').appendTo($game);
+            for (var i = 0; i < stars; i++) {
+                $stars.append('<span>');
+            }
+        }
         if ($game.find('.eyepod').is('*')) return $.when(true);
         var eyepods = [];
         for (var i = 0; i < 9; i++) {
@@ -93,7 +102,9 @@ function HitTheDiamond(selector) {
                     $pod.append($("<div class='gem'>").addClass(gem.toString()).mousedown(function () {
                         if ($(this).is(".hit")) return;
                         $(this).removeClass('popped').addClass('hit');
-                        addScore(gem.score);
+                        gem.hits++;
+                        if (gem.score > 0) addScore(gem.score);
+                        else addFail();
                     }).append($("<img>").attr('src', gem.img)).append($("<img>").attr('src', gem.hitImg)));
                     deferred.resolve();
                 }, 1);
@@ -104,8 +115,11 @@ function HitTheDiamond(selector) {
 
     function StartGame() {
         addScore(score = 0);
+        fails = 0;
+        gems.forEach(function (gem) { gem.hits = 0; });
         $game.addClass("running").find('.gem').removeClass('popped hit');
-        Setup().then(function () { setTimeout(function () { pop(1500); }, 1000); });
+        $game.find('.stars .used').removeClass('used');
+        setTimeout(function () { pop(1500); }, 1000);
     }
 
     var popnums = [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 4];
@@ -133,14 +147,20 @@ function HitTheDiamond(selector) {
         setTimeout(function () { pop(interval); }, interval);
     }
 
+    function gemIconMaker(imgProp, infoFunc) {
+        imgProp = imgProp || "img";
+        infoFunc = infoFunc || function (gem) { return gem.score + " points"; };
+        return function (gem) {
+            return $('<li>').append($('<div class="icon">').addClass(gem.toString()).addClass(imgProp).css('background-image', 'url("' + (gem[imgProp] || gem.img) + '")')).append($('<div>').text(gem.name)).append($('<div>').text(infoFunc(gem)));
+        }
+    }
+
     var introtext = "The Roaming Eyes have landed! Nobody knows what lurks inside, but the Crystal Gems have gone in to investigate. Your mission: without endangering the Crystal Gems, keep whatever gems that arrived inside the Roaming Eyes from escaping! That's right, it's time to <i>Hit The Diamond!</i>";
-    var instructions = $("<span>").text("Gems will pop out of the doors. If they're not Garnet, Amethyst, Pearl, Peridot, or Lapis Lazuli, click on them to knock them back down.");
-    instructions.append($("<ul>").append(gems.map(function (gem) {
-        return $('<li>').text(gem.name + ": " + gem.score + " points");
-    })));
-    var gameovertext = "Game Over. Score: %S. %C!";
+    var instructions = $("<span>").text("Gems will pop out of the doors. If they're not Garnet, Amethyst, Pearl, Peridot, or Lapis Lazuli, click on them to knock them back down. If you hit a Crystal Gem, it'll cost you a star. The game ends when all the stars are gone. Good luck!");
+    instructions.append($("<ul class='instructions'>").append(gems.filter(function (g) { return g.score > 0 }).map(gemIconMaker())));
+    var gameovertext = "You scored %S points. %H Here's the damage:";
     var credits = $("<span>").text("Credits").append($("<ul>").addClass("credits").append([
-        { credit: "Programming", by: "Dave", link:"http://www.beachcitybugle.com/" },
+        { credit: "Programming", by: "Dave", link: "http://www.beachcitybugle.com/" },
         { credit: "Art", by: "Steven Universe", link: "http://www.cartoonnetwork.com/video/steven-universe/" },
         { credit: "Crewniverse Font", by: "MaxiGamer", link: "http://fav.me/d8xkpe8" },
         { credit: "Special Thanks", by: "Steven Universe Wiki", link: "http://steven-universe.wikia.com/" }
@@ -159,18 +179,35 @@ function HitTheDiamond(selector) {
     }));
 
     function GameOver() {
+        if (score > highscore) {
+            highscore = score;
+            window.localStorage.setItem("htd-highscore", highscore);
+        }
         $game.addClass('over').removeClass('running').find('.gem').removeClass('popped');
-        Dialog(gameovertext.replace('%S', score).replace('%C', "Yay"), "Play Again", "Credits").then(function (s) { return s; }, function (f) {
+        var text = $('<span>').text(gameovertext.replace('%S', score).replace('%H', highscore == score ? "That's a new personal record!" : "Nice work."))
+            .append($('<ul class="instructions">').append(gems.filter(function (g) {
+                return g.hits > 0;
+            }).sort(function (a, b) {
+                return b.hits - a.hits;
+            }).map(gemIconMaker("hitImg", function (gem) {
+                return gem.hits + " hit" + (gem.hits > 1 ? "s" : "");
+            }))));
+        Dialog(text, "Play Again", "Credits").then(function (s) { return s; }, function (f) {
             return Dialog(credits, "Play Again");
         }).then(Intro);
     }
 
     function Intro() {
-        $game.removeClass('over');
+        $game.removeClass('over').attr('data-highscore', highscore);
         Dialog(introtext, "Start Game", "Instructions", preLoad).then(function (s) { return s; }, function (f) {
             return Dialog(instructions, "Start Game", preLoad);
-        }).then(StartGame);
+        }).then(Setup).then(StartGame);
     }
 
-    Intro();
+    if (typeof (requestAnimationFrame) === "function") {
+        Intro();
+    }
+    else {
+        Dialog("Sorry, it looks like this game might not support your web browser. If at all possible, please consider <a href='https://browser-update.org/update.html' target='_blank'>updating</a> your browser.", "Try to Play Anyway", "Dismiss").then(Intro);
+    }
 }
