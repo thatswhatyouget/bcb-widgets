0af6c6bd304746c39078cce933ab60bba4371345
Dave Rhodes <dave.was.r@gmail.com>
2016-05-31 00:09:45 +0000

    upated credits again

--- hitthediamond/htd.js
+++ hitthediamond/htd.js
@@ -199,7 +199,7 @@ function HitTheDiamond(selector) {
     var gameovertext = "You scored %S points. %H Here's the damage:";
     var credits = $("<span>").text("Credits").append($("<ul>").addClass("credits").append([
         { credit: "Design, Programming", by: "Dave", link: "http://www.beachcitybugle.com/" },
-        { credit: "Art, Sounds", by: "Steven Universe", link: "http://www.cartoonnetwork.com/video/steven-universe/" },
+        { credit: "Art, Sound", by: "Steven Universe", link: "http://www.cartoonnetwork.com/video/steven-universe/" },
         { credit: "Crewniverse Font", by: "MaxiGamer", link: "http://fav.me/d8xkpe8" },
         { credit: "Yellow Pearl Vector", by: "Deco-kun", link: "http://steven-universe.wikia.com/wiki/User:Deco-kun" },
         { credit: "Special Thanks", by: "Steven Universe Wiki", link: "http://steven-universe.wikia.com/" }
