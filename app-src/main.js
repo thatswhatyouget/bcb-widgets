b1d8e86384a7711a738253dc90eb71f9e85b9c81
Dave Rhodes <dave.was.r@gmail.com>
2017-01-12 04:00:25 +0000

    Updated app frame size to dodge horizontal scroll bar

--- app-src/main.js
+++ app-src/main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 960, height: 720})
+  win = new BrowserWindow({width: 1024, height: 768})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
