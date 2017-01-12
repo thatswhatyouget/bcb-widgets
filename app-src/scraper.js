ad421b000a8e89ed8aa7805c1037f0438361861e
Dave Rhodes <dave.was.r@gmail.com>
2017-01-12 06:51:05 +0000

    Vastly improved in-app scraper

--- app-src/scraper.js
+++ app-src/scraper.js
@@ -1,24 +1,13 @@
-var http = require('http');
+var request = require('request');
 exports.scrape = function (url) {
     return new Promise(function (resolve, reject) {
-        var urlParts = url.split('/').filter(p=>!!p);
-        urlParts.shift(); //pop off protocol
-        var options = {
-            host: urlParts.shift(),
-            port: 80,
-            path: '/' + urlParts.join('/')
-        };
-        http.get(options, function (response) {
-            var data = "";
-            response.on('data', function (chunk) {
-                data += chunk;
-            });
-            response.on('end', function () {
-                if (data) resolve(data);
-                else reject("Response was empty");
-            });
-        }).on('error', function (e) {
-            reject(e.message);
-        });
+        request(url, function (error, response, body) {
+            if (!error && response.statusCode == 200 && body) {
+                resolve(body);
+            }
+            else {
+                reject(response.statusCode + ": " + response.statusMessage);
+            }
+        })
     });
 }
